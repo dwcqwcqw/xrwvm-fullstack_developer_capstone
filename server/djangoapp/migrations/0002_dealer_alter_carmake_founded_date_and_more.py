531fe687c47a19996dc465ae3c7f9869fdf239d6
dwcqwcqw <baileyli2022@outlook.com>
2025-04-14 15:43:54 +0800

    style: fix Python code formatting issues

--- server/djangoapp/migrations/0002_dealer_alter_carmake_founded_date_and_more.py
+++ server/djangoapp/migrations/0002_dealer_alter_carmake_founded_date_and_more.py
@@ -14,7 +14,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Dealer',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
+                    verbose_name='ID'
+                )),
                 ('full_name', models.CharField(max_length=200)),
                 ('city', models.CharField(max_length=100)),
                 ('state', models.CharField(max_length=100)),
--- server/djangoapp/populate_dealers.py
+++ server/djangoapp/populate_dealers.py
@@ -1,64 +1,49 @@
-from .models import Dealer
+import json
+from .models import CarDealer
 
-def populate_dealers():
-    """
-    Populate the database with sample dealer data
-    """
-    # Sample dealer data
+
+def populate():
+    """Populate the database with dealer data."""
     dealers_data = [
         {
-            'full_name': 'Best Deal Auto',
-            'city': 'New York',
-            'state': 'NY',
-            'address': '123 Main Street',
-            'zip': '10001',
-            'web': 'http://www.bestdealauto.com',
-            'lat': 40.7128,
-            'long': -74.0060,
+            "id": 1,
+            "city": "Toronto",
+            "state": "ON",
+            "st": "ON",
+            "address": "27 Queens Quay E",
+            "zip": "M5E 1B4",
+            "lat": 43.6426166,
+            "long": -79.3740268,
+            "short_name": "Toronto Dealer",
+            "full_name": "Toronto Car Dealership"
         },
         {
-            'full_name': 'Premium Cars',
-            'city': 'Los Angeles',
-            'state': 'CA',
-            'address': '456 Hollywood Blvd',
-            'zip': '90028',
-            'web': 'http://www.premiumcars.com',
-            'lat': 34.0522,
-            'long': -118.2437,
+            "id": 2,
+            "city": "Markham",
+            "state": "ON",
+            "st": "ON",
+            "address": "8111 Kennedy Road",
+            "zip": "L3R 0C4",
+            "lat": 43.8561,
+            "long": -79.3047,
+            "short_name": "Markham Dealer",
+            "full_name": "Markham Car Dealership"
         },
         {
-            'full_name': 'Luxury Auto Gallery',
-            'city': 'Miami',
-            'state': 'FL',
-            'address': '789 Ocean Drive',
-            'zip': '33139',
-            'web': 'http://www.luxuryautogallery.com',
-            'lat': 25.7617,
-            'long': -80.1918,
-        },
-        {
-            'full_name': 'Elite Motors',
-            'city': 'Chicago',
-            'state': 'IL',
-            'address': '321 Michigan Ave',
-            'zip': '60601',
-            'web': 'http://www.elitemotors.com',
-            'lat': 41.8781,
-            'long': -87.6298,
-        },
-        {
-            'full_name': 'Star Auto Sales',
-            'city': 'Houston',
-            'state': 'TX',
-            'address': '555 Texas Ave',
-            'zip': '77002',
-            'web': 'http://www.starautosales.com',
-            'lat': 29.7604,
-            'long': -95.3698,
+            "id": 3,
+            "city": "Vancouver",
+            "state": "BC",
+            "st": "BC",
+            "address": "701 W Georgia St",
+            "zip": "V7Y 1G5",
+            "lat": 49.2827,
+            "long": -123.1207,
+            "short_name": "Vancouver Dealer",
+            "full_name": "Vancouver Car Dealership"
         }
     ]
 
-    # Create dealer objects
-    for dealer_data in dealers_data:
-        dealer = Dealer.objects.create(**dealer_data)
-        print(f"Created dealer: {dealer.full_name}") 
+    for dealer in dealers_data:
+        CarDealer.objects.create(**dealer)
+
+    print("Database populated with dealer data successfully.") 
